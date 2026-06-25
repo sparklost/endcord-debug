27d533dfca27c1b53172633d9870e75984343ce0
sparklost <marko.b.zivic@gmail.com>
2026-06-25 20:45:45 +0200

    test 12

--- tools/build_python.sh
+++ tools/build_python.sh
@@ -45,8 +45,8 @@ LDFLAGS="${LDFLAGS} \
     -Wl,--exclude-libs,ALL \
     -Wl,-rpath,$PREFIX/lib \
     -LModules/_hacl"
-[[ "$2" == "clang" ]] && CFLAGS="${CFLAGS} -flto=thin"
-[[ "$2" == "clang" ]] && LDFLAGS="${LDFLAGS} -flto=thin"
+[[ "$2" == "clang" ]] && CFLAGS="${CFLAGS} -flto=thin -fuse-ld=lld"
+[[ "$2" == "clang" ]] && LDFLAGS="${LDFLAGS} -flto=thin -fuse-ld=lld"
 export CFLAGS LDFLAGS
 
 cat > ./Modules/Setup.local << EOF
